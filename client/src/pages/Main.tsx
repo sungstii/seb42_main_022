7f272b917469cc8640123302d78b869dabf2b499
jeongminsang <101001956+jeongminsang@users.noreply.github.com>
2023-03-17 16:30:25 +0900

    캐러셀 수정

--- client/src/pages/Main.tsx
+++ client/src/pages/Main.tsx
@@ -2,12 +2,16 @@ import React from "react";
 import { Reset } from "styled-reset";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
+import Carousel from '../components/Carousel';
+import ShortcutCard from '../components/ShortcutCard';
 
 const Main = () => {
   return (
     <>
       <Reset />
       <Header />
+      <Carousel/>
+      <ShortcutCard/>
       <Footer />
     </>
   );
