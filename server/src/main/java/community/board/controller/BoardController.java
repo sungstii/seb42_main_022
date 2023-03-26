4813b639575a4cac08497fa7e46f2c097f9bf7ba
kjsu1994 <kjsu1994@gmail.com>
2023-03-27 05:44:22 +0900

    fix : 배포 자동화를 위한 yml설정 변경

--- server/src/main/java/community/board/controller/BoardController.java
+++ server/src/main/java/community/board/controller/BoardController.java
@@ -118,7 +118,7 @@ public class BoardController {
                                              @RequestParam(required = false) String searchValue,
                                              @PageableDefault(size = 100, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable) throws Exception //페이지 기본값
     {
-        boardService.nameAndLevelUpdate(); //작성자들의 레벨 및 이름 업데이트
+        boardService.nameAndLevelUpdate(); //작성자들의 레벨 및 이름 업데이트 /검색하는사람의 토큰값으로 확인가능
 
         Page<Board> boardPage = boardService.findBoards(Board.KindOfBoard.ECO_REVIEW, searchType, searchValue, pageable);
         List<Board> boards = boardPage.getContent();
